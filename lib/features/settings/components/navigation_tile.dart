4ac1f3f5cb4bc0efb395e2feaf5663403df6fc52
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-10-26 20:49:04 +0200

    refactoring and completed navigation and switch

--- lib/features/settings/components/navigation_tile.dart
+++ lib/features/settings/components/navigation_tile.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:settings_ui/settings_ui.dart';
 
-class NavigationTile extends AbstractSettingsTile  {
+class NavigationTile extends AbstractSettingsTile {
   final Widget leading;
   final String title;
   final String? description;
   final String? toolTipText;
+  final bool enabled;
   final dynamic Function(BuildContext)? onPressed;
-  const NavigationTile(this.onPressed, {super.key, required this.leading, required this.title, this.toolTipText, required this.description});
+  const NavigationTile(this.onPressed,
+      {super.key,
+      required this.leading,
+      required this.title,
+      this.toolTipText,
+      this.description,
+      this.enabled = true});
 
   @override
   Widget build(BuildContext context) {
-    return toolTipText != null ? Tooltip(
-      message: toolTipText,
-      margin: const EdgeInsets.only(top: 8),
-      child: _switch(context),
-    ) : _switch(context);
+    return toolTipText != null
+        ? Tooltip(
+            message: toolTipText,
+            margin: const EdgeInsets.only(top: 8, left: 16, right: 16),
+            child: _switch(context),
+          )
+        : _switch(context);
   }
 
   Widget _switch(BuildContext context) {
@@ -25,6 +34,7 @@ class NavigationTile extends AbstractSettingsTile  {
       title: PlatformText(title),
       description: description != null ? PlatformText(description!) : null,
       onPressed: onPressed,
+      enabled: enabled,
     );
   }
 }
--- lib/features/settings/platform_settings.dart
+++ lib/features/settings/platform_settings.dart
@@ -1,11 +1,15 @@
+import 'package:abs_flutter/features/settings/components/navigation_tile.dart';
 import 'package:abs_flutter/features/settings/components/switch_tile.dart';
 import 'package:abs_flutter/generated/l10n.dart';
 import 'package:abs_flutter/globals.dart';
 import 'package:abs_flutter/provider/settings_provider.dart';
+import 'package:abs_flutter/provider/user_provider.dart';
 import 'package:abs_flutter/util/constants.dart';
+import 'package:abs_flutter/util/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:settings_ui/settings_ui.dart';
 
@@ -21,50 +25,197 @@ class PlatformSettings extends ConsumerWidget {
       appBar: PlatformAppBar(
         title: PlatformText(S.of(context).settings),
       ),
-      body: StatefulBuilder(
-        builder: (context, setState) {
-          return SettingsList(
-            sections: [
-              SettingsSection(
-                title: PlatformText(S.of(context).general),
-                tiles: [
-                  SwitchTile(leading: Icon(PlatformIcons(context).brightness), title: S.of(context).darkMode, keyValue: Constants.DARK_MODE, defaultValue: true),
-                  //TODO: Language
-                  SwitchTile(leading: const Icon(Clarity.switch_line), title: S.of(context).showAccountSwitcher, keyValue: Constants.ACCOUNT_SWITCHER, defaultValue: true, toolTipText: S.of(context).showAccountSwitcherDescription,),
-                  //TODO: markItemsFInished (SLider)
-                  SwitchTile(leading: const Icon(
-                      OctIcons.sidebar_collapse,
+      body: StatefulBuilder(builder: (context, setState) {
+        return SettingsList(
+          sections: [
+            SettingsSection(
+              title: PlatformText(S.of(context).general),
+              tiles: [
+                SwitchTile(
+                    leading: Icon(PlatformIcons(context).brightness),
+                    title: S.of(context).darkMode,
+                    keyValue: Constants.DARK_MODE,
+                    defaultValue: true),
+                //TODO: Language
+                SwitchTile(
+                  leading: const Icon(Clarity.switch_line),
+                  title: S.of(context).showAccountSwitcher,
+                  keyValue: Constants.ACCOUNT_SWITCHER,
+                  defaultValue: true,
+                  toolTipText: S.of(context).showAccountSwitcherDescription,
+                ),
+                //TODO: markItemsFInished (SLider)
+                SwitchTile(
+                  leading: const Icon(
+                    OctIcons.sidebar_collapse,
                     size: 18,
-                  ), title: S.of(context).collapseSeries, keyValue: Constants.COLLAPSE_SERIES, defaultValue: false, toolTipText: S.of(context).collapseSeries,),
-                  SwitchTile(leading: const Icon(Clarity.wifi_line), title: S.of(context).downloadsOnlyViaWifi, keyValue: Constants.DOWNLOADS_ONLY_VIA_WIFI, defaultValue: false),
-                  SwitchTile(leading: const Icon(Clarity.sync_line), title: S.of(context).syncOnlyViaWifi, keyValue: Constants.SYNC_ONLY_VIA_WIFI, defaultValue: false),
-                ],
-              ),
-              SettingsSection(
-                  title: PlatformText(S.of(context).playerSettings),
-                tiles: [
-                  SwitchTile(leading: const Icon(Iconsax.stop_outline), title: S.of(context).stopPlayerUntilSync, keyValue: Constants.STOP_PLAYER_WHILE_SYNCING, defaultValue: true, toolTipText: S.of(context).stopPlayerWhileSyncingDescription),
-                  SwitchTile(leading: Icon(PlatformIcons(context).clockSolid), title: S.of(context).jumpToLastPosition, keyValue: Constants.JUMP_TO_LAST_POSITION, defaultValue: true, toolTipText: S.of(context).jumpToLastPositionDescription),
-                  SwitchTile(leading: const Icon(FontAwesome.bars_progress_solid), title: S.of(context).showProgressPerChapters, keyValue: Constants.PROGRESS_AS_CHAPTERS, defaultValue: false, toolTipText: S.of(context).progressAsChaptersDescription,),
-                  SwitchTile(leading: const Icon(AntDesign.shake_outline), title: S.of(context).shakeResetTimer, keyValue: Constants.SHAKE_RESET_TIMER, defaultValue: false),
-                  SwitchTile(leading: const Icon(OctIcons.lock), title: S.of(context).lockProgressBar, keyValue: Constants.LOCK_SEEKING_NOTIFICATION, defaultValue: false, toolTipText: S.of(context).lockProgressBarDescription,), // TODO: Show Restart Info
-                ],
-              ),
-              SettingsSection(
-                title: PlatformText(S.of(context).cachingHeader),
-                tiles: [
-                  SwitchTile(leading: const Icon(Icons.cached_outlined), title: S.of(context).caching, keyValue: Constants.CACHING_ENABLED, defaultValue: true),
-                  SwitchTile(leading: const Icon(Icons.rocket_launch_outlined), title: S.of(context).aggressiveCaching, keyValue: Constants.AGGRESSIVE_CACHING, defaultValue: true, toolTipText: S.of(context).aggressiveCachingDescription),
-                  SwitchTile(leading: const Icon(Icons.rocket_outlined), title: S.of(context).boostLoading, keyValue: Constants.BOOST_LOADING, defaultValue: false, toolTipText: S.of(context).boostLoadingDescription),
-
-                ],
-              )
-            ],
-          );
-        }
-      ),
+                  ),
+                  title: S.of(context).collapseSeries,
+                  keyValue: Constants.COLLAPSE_SERIES,
+                  defaultValue: false,
+                  toolTipText: S.of(context).collapseSeries,
+                ),
+                SwitchTile(
+                    leading: const Icon(Clarity.wifi_line),
+                    title: S.of(context).downloadsOnlyViaWifi,
+                    keyValue: Constants.DOWNLOADS_ONLY_VIA_WIFI,
+                    defaultValue: false),
+                SwitchTile(
+                    leading: const Icon(Clarity.sync_line),
+                    title: S.of(context).syncOnlyViaWifi,
+                    keyValue: Constants.SYNC_ONLY_VIA_WIFI,
+                    defaultValue: false),
+              ],
+            ),
+            SettingsSection(
+              title: PlatformText(S.of(context).playerSettings),
+              tiles: [
+                SwitchTile(
+                    leading: const Icon(Iconsax.stop_outline),
+                    title: S.of(context).stopPlayerUntilSync,
+                    keyValue: Constants.STOP_PLAYER_WHILE_SYNCING,
+                    defaultValue: true,
+                    toolTipText:
+                        S.of(context).stopPlayerWhileSyncingDescription),
+                SwitchTile(
+                    leading: Icon(PlatformIcons(context).clockSolid),
+                    title: S.of(context).jumpToLastPosition,
+                    keyValue: Constants.JUMP_TO_LAST_POSITION,
+                    defaultValue: true,
+                    toolTipText: S.of(context).jumpToLastPositionDescription),
+                SwitchTile(
+                  leading: const Icon(FontAwesome.bars_progress_solid),
+                  title: S.of(context).showProgressPerChapters,
+                  keyValue: Constants.PROGRESS_AS_CHAPTERS,
+                  defaultValue: false,
+                  toolTipText: S.of(context).progressAsChaptersDescription,
+                ),
+                SwitchTile(
+                    leading: const Icon(AntDesign.shake_outline),
+                    title: S.of(context).shakeResetTimer,
+                    keyValue: Constants.SHAKE_RESET_TIMER,
+                    defaultValue: false),
+                SwitchTile(
+                  leading: const Icon(OctIcons.lock),
+                  title: S.of(context).lockProgressBar,
+                  keyValue: Constants.LOCK_SEEKING_NOTIFICATION,
+                  defaultValue: false,
+                  toolTipText: S.of(context).lockProgressBarDescription,
+                ), // TODO: Show Restart Info
+              ],
+            ),
+            SettingsSection(
+              title: PlatformText(S.of(context).cachingHeader),
+              tiles: [
+                SwitchTile(
+                    leading: const Icon(Icons.cached_outlined),
+                    title: S.of(context).caching,
+                    keyValue: Constants.CACHING_ENABLED,
+                    defaultValue: true),
+                SwitchTile(
+                    leading: const Icon(Icons.rocket_launch_outlined),
+                    title: S.of(context).aggressiveCaching,
+                    keyValue: Constants.AGGRESSIVE_CACHING,
+                    defaultValue: true,
+                    toolTipText: S.of(context).aggressiveCachingDescription),
+                SwitchTile(
+                    leading: const Icon(Icons.rocket_outlined),
+                    title: S.of(context).boostLoading,
+                    keyValue: Constants.BOOST_LOADING,
+                    defaultValue: false,
+                    toolTipText: S.of(context).boostLoadingDescription),
+                NavigationTile(
+                  (BuildContext context) => Helper.clearCache(),
+                  leading: Icon(PlatformIcons(context).delete),
+                  title: S.of(context).clearCache,
+                ),
+                SwitchTile(
+                    leading: const Icon(Icons.developer_mode),
+                    title: S.of(context).logging,
+                    keyValue: Constants.LOGGING_ENABLED,
+                    defaultValue: true),
+              ],
+            ),
+            SettingsSection(
+              title: PlatformText(S.of(context).user),
+              tiles: [
+                NavigationTile(
+                  (BuildContext context) => context.push('/select-server'),
+                  leading: Icon(PlatformIcons(context).personAdd),
+                  title: S.of(context).addANewUser,
+                ),
+                ..._getOtherUsers(ref, context),
+                NavigationTile(
+                  (BuildContext context) => ref
+                      .watch(currentUserProvider.notifier)
+                      .removeUser(context),
+                  leading: Icon(PlatformIcons(context).exitToApp),
+                  title: S.of(context).signOut,
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: PlatformText(S.of(context).miscellaneous),
+              tiles: [
+                NavigationTile(
+                  (BuildContext context) => Helper.launchUrl(
+                      'https://github.com/vito0912/abs_flutter'),
+                  title: S.of(context).viewOnGithub,
+                  leading: const Icon(Bootstrap.github),
+                ),
+                NavigationTile(
+                  (BuildContext context) => Helper.launchUrl(
+                      'https://github.com/Vito0912/abs_flutter/issues/new'),
+                  title: S.of(context).reportAnIssue,
+                  leading: const Icon(Bootstrap.github),
+                ),
+                NavigationTile(
+                  (BuildContext context) =>
+                      Helper.launchUrl('https://www.audiobookshelf.org/'),
+                  title: S.of(context).openProjectLink,
+                  leading: Helper.returnLibraryItem('audiobookshelf', context),
+                ),
+                NavigationTile(
+                  (BuildContext context) => showLicensePage(
+                    context: context,
+                    applicationName: appTitle,
+                    applicationVersion: version,
+                    applicationLegalese: 'Vito',
+                  ),
+                  title: S.of(context).attribution,
+                  leading: Icon(PlatformIcons(context).info),
+                ),
+                NavigationTile(
+                  (BuildContext context) => null,
+                  title:
+                      S.of(context).deviceInfo(deviceName, osVersion, version),
+                  leading: Icon(PlatformIcons(context).info),
+                ),
+              ],
+            )
+          ],
+        );
+      }),
     );
   }
 
+  List<NavigationTile> _getOtherUsers(WidgetRef ref, BuildContext context) {
+    final users = ref.read(usersProvider);
+    final selectedUser = ref.read(selectedUserProvider);
 
+    return users.asMap().entries.map((entry) {
+      final index = entry.key;
+      final user = entry.value;
+      return NavigationTile(
+        (BuildContext context) {
+          ref.watch(selectedUserProvider.notifier).state = index;
+          context.pop();
+        },
+        title: user.username ?? '',
+        description: user.server?.url ?? '',
+        leading: Icon(PlatformIcons(context).person),
+        enabled: index != selectedUser,
+      );
+    }).toList();
+  }
 }
