1a2dac82571269a8408d76fb49326524ca665ad3
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-10-28 20:05:58 +0100

    Added ScrollingText for title
    Made the minified player a little bit better

--- lib/features/player/player_minified.dart
+++ lib/features/player/player_minified.dart
@@ -6,6 +6,7 @@ import 'package:abs_flutter/provider/player_status_provider.dart';
 import 'package:abs_flutter/provider/user_provider.dart';
 import 'package:abs_flutter/util/helper.dart';
 import 'package:abs_flutter/widgets/album_image.dart';
+import 'package:abs_flutter/widgets/scrolling_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -82,17 +83,14 @@ class PlayerMinified extends ConsumerWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   mainAxisSize: MainAxisSize.min,
                                   children: [
-                                    PlatformText(
-                                      player.audioService.mediaItem.value!
-                                              .title +
-                                          (currentChapter != null
-                                              ? ' - ${currentChapter.title}'
-                                              : ''),
+                                    ScrollingText(
+                                      text: player
+                                          .audioService.mediaItem.value!.title,
                                       style: Theme.of(context)
                                           .textTheme
-                                          .labelSmall,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 1,
+                                          .labelMedium!,
+                                      scrollDuration:
+                                          const Duration(seconds: 5),
                                     ),
                                     PlatformText(
                                       player.audioService.mediaItem.value!
@@ -110,38 +108,16 @@ class PlayerMinified extends ConsumerWidget {
                             ],
                           ),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SeekingButtons(
-                              positionStream: positionStream,
-                              player: player,
-                              isForward: false,
-                            ),
-                            PlayButton(
-                                playerStatusProvider: playerStatusProvider),
-                            SeekingButtons(
-                              positionStream: positionStream,
-                              player: player,
-                              isForward: true,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                                onPressed: () {
-                                  if (Helper.getCurrentRoute(
-                                          GoRouter.of(context)) !=
-                                      '/settings') {
-                                    context.push('/settings');
-                                  }
-                                },
-                                icon: const Icon(Icons.more_vert)),
-                          ],
-                        ),
+                        MediaQuery.of(context).size.width > 500
+                            ? Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                child: _buildControls(positionStream, player,
+                                    playerStatusProvider))
+                            : _buildControls(
+                                positionStream, player, playerStatusProvider),
+                        MediaQuery.of(context).size.width > 500
+                            ? Flexible(child: _buildOptions(context))
+                            : _buildOptions(context),
                       ],
                     ),
                   ProgressBar(
@@ -159,6 +135,42 @@ class PlayerMinified extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildControls(positionStream, player, playerStatusProvider) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SeekingButtons(
+          positionStream: positionStream,
+          player: player,
+          isForward: false,
+        ),
+        PlayButton(playerStatusProvider: playerStatusProvider),
+        SeekingButtons(
+          positionStream: positionStream,
+          player: player,
+          isForward: true,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOptions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+            onPressed: () {
+              if (Helper.getCurrentRoute(GoRouter.of(context)) != '/settings') {
+                context.push('/settings');
+              }
+            },
+            icon: const Icon(Icons.more_vert)),
+      ],
     );
   }
 }
