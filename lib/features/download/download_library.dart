e45809829922266cb5aea901a54f6c7741833310
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-12-02 16:18:17 +0100

    fixes download scrolling

--- lib/features/download/download_library.dart
+++ lib/features/download/download_library.dart
@@ -40,111 +40,122 @@ class DownloadLibrary extends HookConsumerWidget {
                     title: Text(libraryName ?? S.of(context).downloads),
                   )
                 : null,
-            body:
-                _listBuilder(libraryDownloads, context, ref, selectedDownloads),
+            body: SingleChildScrollView(
+                child: _listBuilder(
+                    libraryDownloads, context, ref, selectedDownloads)),
           )
         : _listBuilder(libraryDownloads, context, ref, selectedDownloads);
   }
 
   Widget _listBuilder(List<DownloadInfo> libraryDownloads, BuildContext context,
       WidgetRef ref, ValueNotifier<List<DownloadInfo>> selectedDownloads) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-      if (selectedDownloads.value.isNotEmpty)
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: PlatformElevatedButton(
-            child: PlatformText(S.of(context).deleteSelected),
-            onPressed: () =>
-                _deleteSelectedDownloads(ref, selectedDownloads.value),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (selectedDownloads.value.isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PlatformElevatedButton(
+              child: PlatformText(S.of(context).deleteSelected),
+              onPressed: () =>
+                  _deleteSelectedDownloads(ref, selectedDownloads.value),
+            ),
+          ),
+        SingleChildScrollView(
+          child: ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: libraryDownloads.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(bottom: 8),
+            itemBuilder: (context, index) {
+              final item = libraryDownloads[index];
+              final itemType = item.type.name;
+              final isSelected = selectedDownloads.value.contains(item);
+
+              return ListTile(
+                title: Text(item.displayName),
+                subtitle: Text('${item.files.length.toString()} media files'),
+                onTap: () {
+                  context.push('/view/$itemType/${item.itemId}');
+                },
+                leading: AlbumImage(
+                  item.itemId,
+                  size: 48,
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    PlatformIconButton(
+                      icon: Icon(
+                        isSelected
+                            ? Icons.check_box
+                            : Icons.check_box_outline_blank,
+                      ),
+                      onPressed: () =>
+                          _toggleSelection(item, selectedDownloads),
+                    ),
+                    PlatformIconButton(
+                      icon: Icon(PlatformIcons(context).deleteOutline),
+                      onPressed: () {
+                        ref
+                            .read(downloadListProvider.notifier)
+                            .removeDownload(item);
+                      },
+                    ),
+                    PlatformIconButton(
+                        icon: Icon(PlatformIcons(context).info),
+                        onPressed: () {
+                          showPlatformDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return PlatformAlertDialog(
+                                  title:
+                                      PlatformText(S.of(context).downloadInfo),
+                                  content: SingleChildScrollView(
+                                    child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          PlatformText(
+                                              '${S.of(context).libraryName}: ${item.libraryName}'),
+                                          PlatformText(
+                                              '${S.of(context).itemId}: ${item.itemId}'),
+                                          if (item.episodeId != null)
+                                            PlatformText(
+                                                '${S.of(context).episodeId}: ${item.episodeId}'),
+                                          const Divider(),
+                                          ..._buildFileText(item, context),
+                                          const Divider(),
+                                          PlatformText(
+                                              '${S.of(context).itemType}: ${item.type.name}'),
+                                        ]
+                                            .map((e) => Padding(
+                                                padding:
+                                                    const EdgeInsets.all(4.0),
+                                                child: e))
+                                            .toList()),
+                                  ),
+                                  actions: [
+                                    PlatformDialogAction(
+                                      child: PlatformText(S.of(context).close),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              });
+                        }),
+                  ],
+                ),
+              );
+            },
           ),
         ),
-      ListView.builder(
-        itemCount: libraryDownloads.length,
-        padding: const EdgeInsets.only(bottom: 8),
-        shrinkWrap: true,
-        itemBuilder: (context, index) {
-          final item = libraryDownloads[index];
-          final itemType = item.type.name;
-          final isSelected = selectedDownloads.value.contains(item);
-
-          return ListTile(
-            title: Text(item.displayName),
-            subtitle: Text('${item.files.length.toString()} media files'),
-            onTap: () {
-              context.push('/view/$itemType/${item.itemId}');
-            },
-            leading: AlbumImage(
-              item.itemId,
-              size: 48,
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                PlatformIconButton(
-                  icon: Icon(
-                    isSelected
-                        ? Icons.check_box
-                        : Icons.check_box_outline_blank,
-                  ),
-                  onPressed: () => _toggleSelection(item, selectedDownloads),
-                ),
-                PlatformIconButton(
-                  icon: Icon(PlatformIcons(context).deleteOutline),
-                  onPressed: () {
-                    ref
-                        .read(downloadListProvider.notifier)
-                        .removeDownload(item);
-                  },
-                ),
-                PlatformIconButton(
-                    icon: Icon(PlatformIcons(context).info),
-                    onPressed: () {
-                      showPlatformDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return PlatformAlertDialog(
-                              title: PlatformText(S.of(context).downloadInfo),
-                              content: SingleChildScrollView(
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      PlatformText(
-                                          '${S.of(context).libraryName}: ${item.libraryName}'),
-                                      PlatformText(
-                                          '${S.of(context).itemId}: ${item.itemId}'),
-                                      if (item.episodeId != null)
-                                        PlatformText(
-                                            '${S.of(context).episodeId}: ${item.episodeId}'),
-                                      const Divider(),
-                                      ..._buildFileText(item, context),
-                                      const Divider(),
-                                      PlatformText(
-                                          '${S.of(context).itemType}: ${item.type.name}'),
-                                    ]
-                                        .map((e) => Padding(
-                                            padding: const EdgeInsets.all(4.0),
-                                            child: e))
-                                        .toList()),
-                              ),
-                              actions: [
-                                PlatformDialogAction(
-                                  child: PlatformText(S.of(context).close),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          });
-                    }),
-              ],
-            ),
-          );
-        },
-      ),
-    ]);
+      ],
+    );
   }
 
   void _toggleSelection(
